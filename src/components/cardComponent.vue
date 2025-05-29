0813a96bb63e74e52d41f57eb8917d0d3f5d44ed
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2025-05-29 19:32:51 -0300

    3 commit

--- src/components/cardComponent.vue
+++ src/components/cardComponent.vue
@@ -1,30 +1,43 @@
 <template>
-  <div
-    class="p-4 text-black shadow-md bg-red-100 hover:bg-gray-200 w-[250px] h-[400px] mx-auto flex flex-col items-center justify-center font-serif"
-  >
-    <img
-      v-if="product.images.length"
-      :src="product.images[0]"
-      :alt="product.title"
-      id="product-image"
-      class="mx-auto mb-4 h-40 object-contain"
-    />
-    <h2 class="text-2xl capitalize text-center">{{ product.title || 'Carregando...' }}</h2>
-  </div>
+    <div
+      v-for="prod in products"
+      :key="prod.id"
+      class="p-4 text-black shadow-md bg-red-100 hover:bg-gray-200 w-[250px] h-[400px] mx-auto flex flex-col items-center justify-center font-serif"
+    >
+      <img
+        v-if="prod.images && prod.images.length"
+        :src="prod.images[0]"
+        :alt="prod.category"
+        class="mx-auto mb-4 h-40 object-contain"
+      />
+      <h2 class="text-2xl capitalize text-center">{{ prod.category || 'Carregando...' }}</h2>
+    </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 
-const product = ref({ images: [], title: "" });
+const products = ref([]);
+
+async function fetchProductById(id) {
+  try {
+    const res = await fetch(`https://dummyjson.com/products/${id}`);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(`Erro ao buscar produto id=${id}:`, err);
+  }
+}
 
 onMounted(async () => {
-  try {
-    const res = await fetch("https://dummyjson.com/products/5");
-    const data = await res.json();
-    product.value = data;
-  } catch (err) {
-    console.error(err);
+  const ids = [1, 6, 11, 16, 43, 48, 78, 83, 88, 93, 99, 113, 118, 121, 137, 154, 159, 162, 167, 172, 177, 182, 185, 190];
+  const prods = [];
+
+  for (const id of ids) {
+    const product = await fetchProductById(id);
+    if (product) prods.push(product);
   }
+
+  products.value = prods;
 });
 </script>
